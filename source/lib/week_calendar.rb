5d9b9e45c9abf252c44f09bbf1ac3799dea19722
ferfebles <fernandofebles@skua.local>
2014-10-10 00:12:59 +0100

    Simple text version in pdf

--- source/lib/week_calendar.rb
+++ source/lib/week_calendar.rb
@@ -1,22 +1,60 @@
 STARTED_AT = Time.now
 require_relative 'working_week'
+require 'prawn'
+require 'prawn/measurement_extensions'
+
+# Returns a string with the month
+# for the provided working_week
+class WeekCalendar
+  def initialize(wweek, mark = '-')
+    @wweek = wweek
+    @mark = mark
+  end
+
+  def text
+    Date::MONTHNAMES[@wweek.month] +
+    @wweek.month_calendar.map do |day|
+      (day.monday? ? left_margin(day) : '  ') +
+      format('%2d', day.mday)
+    end.join
+  end
+
+  def left_margin(day)
+    if @wweek.days.first == day
+      "\n#{@mark} "
+    else
+      "\n  "
+    end
+  end
+end
 
 year = Time.now.year
-100.times { |i|
+100.times do |i|
   wweek = WorkingWeek.new(year, i) rescue break
-  print Date::MONTHNAMES[wweek.month]
-  #print month: wweek.month, week: i
-  #puts days: wweek.days
-  wweek.month_calendar.each { |day|
-    if day.monday?
-      if wweek.days.first == day
-        print "\n> "
-      else
-        print "\n  "
-      end
-    end
-    print "%2d  " % day.mday
-  }
-  puts "\n\n"
-}
+  puts WeekCalendar.new(wweek).text
+  puts
+end
 puts "Done in #{Time.now - STARTED_AT}s"
+
+Prawn::Document.generate('calendar.pdf',
+                         page_size: 'A5',
+                         page_layout: :landscape,
+                         margin: 0) do
+
+  100.times do |i|
+    wweek = WorkingWeek.new(year, i) rescue break
+    start_new_page
+    calendar = "<font name='Courier' size='7'>"
+    calendar << WeekCalendar.new(wweek).text
+    calendar << '</font>'
+    text_box calendar, at: [12.5.cm, 6.75.cm],
+                       width: 6.cm, height: 4.cm,
+                       overflow: :shrink_to_fit,
+                       min_font_size: 2,
+                       inline_format: true,
+                       align: :right,
+                       leading: 2.4.mm
+
+  end
+  puts "Done in #{Time.now - STARTED_AT}s"
+end
