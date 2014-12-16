b570c90e08785894c33fae5b1ae095b1293ce32c
Fernando Febles <fernandofebles@gmail.com>
2014-12-16 22:57:43 +0000

    Weekcalendar A5 refactored as A4

--- source/lib/week_calendarA5.rb
+++ source/lib/week_calendarA5.rb
@@ -1,120 +1,152 @@
+
 STARTED_AT = Time.now
+
 require_relative 'working_week'
+require_relative 'working_week_formatter'
 require 'prawn'
 require 'prawn/measurement_extensions'
 
-# Returns a pdf formatted string with the
-# month calendar, for the provided working_week
-class WeekCalendar
-  def initialize(wweek)
-    @wweek = wweek
+class CalendarDocument < Prawn::Document
+  def initialize(year)
+    @year = year
+    super(page_size: 'A5', margin: 0, page_layout: :landscape)
+    font 'Inconsolata0.ttf', size: 10
+    fill_color '777777'
+    @page_height, @page_width = PDF::Core::PageGeometry::SIZES['A5']
+    @grid = 0.5
+    @dot_size_cm = 0.01.cm
+    @gutter_cm = 1.cm
+    @hmargin_cm = 1.cm
+    @vmargin_cm = 1.4.cm
+    create_stamp_dots
   end
 
-  def pdf_text
-    Date::MONTHNAMES[@wweek.month] +
-    @wweek.month_calendar.map do |day|
-      current_week?(day) ? d_style(tag(day)) : tag(day)
-    end.join
+  attr_accessor :page_width, :page_height, :gutter_cm,
+                :hmargin_cm, :vmargin_cm, :dot_size_cm
+
+  def title
+    text_box "#{@year}\nWeeks", at: [12.5.cm, 3.cm],
+                                width: 6.cm, height: 1.cm, align: :right
   end
 
-  def current_week?(day)
-    @wweek.days.include? day
+  def background_odd
+    stamp_at 'dots', [@gutter_cm + @hmargin_cm - @dot_size_cm, @vmargin_cm]
+    thu_sun
   end
 
-  def d_style(text)
-    "<color rgb='000000'>" + text + '</color>'
+  def background_even
+    stamp_at 'dots', [@hmargin_cm - @dot_size_cm, @vmargin_cm]
+    mon_wed
   end
 
-  def tag(day)
-    (day.monday? ? "\n" : '  ') +
-    format('%2d', day.mday)
+  def mon_wed
+    xd = 0
+    ['mon', 'tue', 'wed'].each do |day|
+      day_box day, @hmargin_cm + (5 + xd).cm, @page_height - @vmargin_cm
+      xd += 6
+    end
   end
-end
 
-page_height, page_width = PDF::Core::PageGeometry::SIZES['A5']
-grid = 0.5
-dot_size_cm = 0.015.cm
-gutter_cm = 1.cm
-hmargin_cm = 1.cm
-vmargin_cm = 1.4.cm
+  def thu_sun
+    xd = - 6
+    ['thu', 'fri', 'sat'].each do |day|
+      xd += 6
+      day_box day,
+              @gutter_cm + @hmargin_cm + (5 + xd).cm,
+              @page_height - @vmargin_cm
+    end
+    day_box 'sun',
+            @gutter_cm + @hmargin_cm + (5 + xd).cm,
+            @page_height - @vmargin_cm - 4.5.cm
+  end
 
-year = Time.now.year
-Prawn::Document.generate('calendar.pdf',
-                         page_size: 'A5',
-                         page_layout: :landscape,
-                         margin: 0) do
-  font 'Inconsolata0.ttf', size: 10
-  fill_color '777777'
-  text_box "#{year}\nWeeks", at: [12.5.cm, 3.cm],
-                             width: 6.cm, height: 1.cm,
-                             align: :right
+  def calendar_column(text)
+    current_color = fill_color
+    calendar_lines = text.split("\n").size - 2
+    self.fill_color = 'ffffff'
+    fill_rectangle([@page_width - @hmargin_cm - (@grid * 9.5).cm,
+                    @vmargin_cm + (@grid * (calendar_lines + 0.5)).cm],
+                   (@grid * 9).cm, (@grid * calendar_lines).cm)
+    self.fill_color = current_color
+    text_box text, at: [@page_width - @hmargin_cm - 6.05.cm,
+                        @page_height - @vmargin_cm],
+                   width: 5.85.cm, height: 12.cm,
+                   inline_format: true,
+                   align: :right, valign: :bottom,
+                   leading: 1.15.mm,
+                   overflow: :shrink_to_fit, min_font_size: 2
+  end
 
-  create_stamp 'dots' do
-    self.fill_color = '555555'
-    (0 .. 6 * 2).step(grid) do |y|
-      (0 .. 6 * 3).step(grid) do |x|
-        radius_cm = case
-                    when x % 6 == 0
-                      dot_size_cm
-                    when x % 1 == 0
-                      dot_size_cm / 2
-                    else
-                      dot_size_cm / 3
-                    end
-        fill_circle [x.cm + dot_size_cm, y.cm + dot_size_cm], radius_cm
+  def create_stamp_dots(name = 'dots', width = 6 * 3, height = 6 * 2)
+    create_stamp name do
+      self.fill_color = '555555'
+      (0..height).step(@grid) do |y|
+        (0..width).step(@grid) do |x|
+          radius_cm = case
+                      when x % 6 == 0
+                        @dot_size_cm
+                      when x % 1 == 0
+                        @dot_size_cm / 2
+                      else
+                        @dot_size_cm / 3
+                      end
+          fill_circle [x.cm + @dot_size_cm, y.cm + @dot_size_cm], radius_cm
+        end
       end
     end
   end
 
-  repeat(->(pg) { pg.even? && pg > 1 }) do
-    text_box 'mon', at: [hmargin_cm + 5.cm, page_height - vmargin_cm],
-                    width: 1.cm, height: 0.45.cm,
-                    align: :center, valign: :center
-    text_box 'tue', at: [hmargin_cm + 11.cm, page_height - vmargin_cm],
-                    width: 1.cm, height: 0.45.cm,
-                    align: :center, valign: :center
-    text_box 'wed', at: [hmargin_cm + 17.cm, page_height - vmargin_cm],
-                    width: 1.cm, height: 0.45.cm,
-                    align: :center, valign: :center
-    stamp_at 'dots', [hmargin_cm - dot_size_cm, vmargin_cm]
+  
+  def day_box(text, x = 0, y = 0)
+    text_box text, at: [x, y], width: (@grid * 2).cm,
+                   height: (@grid + 0.05).cm,
+                   align: :center, valign: :center
   end
-
-  repeat(->(pg) { pg.odd? && pg > 2 }) do
-    text_box 'thu', at: [gutter_cm + hmargin_cm + 5.cm, page_height - vmargin_cm],
-                    width: 1.cm, height: 0.45.cm,
-                    align: :center, valign: :center
-    text_box 'fri', at: [gutter_cm + hmargin_cm + 11.cm, page_height - vmargin_cm],
-                    width: 1.cm, height: 0.45.cm,
-                    align: :center, valign: :center
-    text_box 'sat', at: [gutter_cm + hmargin_cm + 17.cm, page_height - vmargin_cm],
-                    width: 1.cm, height: 0.45.cm,
-                    align: :center, valign: :center
-    text_box 'sun', at: [gutter_cm + hmargin_cm + 17.cm, page_height - vmargin_cm - 4.5.cm],
-                    width: 1.cm, height: 0.45.cm,
-                    align: :center, valign: :center
-    stamp_at 'dots', [gutter_cm + hmargin_cm - dot_size_cm, vmargin_cm]
-  end
-  year_calendar = ''
-  100.times do |i|
-    wweek = WorkingWeek.new(year, i) rescue break
-    year_calendar << wweek.days.map { |d| format('%2d', d.mday) }.join('  ') + "\n"
-    start_new_page
-    start_new_page
-    calendar = WeekCalendar.new(wweek).pdf_text
-    # puts calendar + "\n"
-    text_box calendar, at: [hmargin_cm + 12.75.cm, vmargin_cm + 4.04.cm],
-                       width: 6.cm, height: 4.cm,
-                       inline_format: true,
-                       align: :right, valign: :bottom,
-                       leading: 1.15.mm
-
-  end
-  start_new_page
-  text_box year_calendar, at: [hmargin_cm + 11.75.cm, page_height - vmargin_cm],
-                     width: 6.cm, height: page_height - vmargin_cm * 2,
-                     inline_format: true,
-                     align: :right, valign: :bottom,
-                     overflow: :shrink_to_fit, min_font_size: 2
-
 end
-puts "Done in #{Time.now - STARTED_AT}s"
+
+year = (ARGV[0] || Time.now.year).to_i
+year_calendar = []
+p = CalendarDocument.new(year)
+
+# First page
+p.title
+
+# Calendar pages
+i = 0
+loop do
+  wweek = WorkingWeek.new(year, i) rescue break
+
+  p.start_new_page
+  p.background_even
+
+  p.start_new_page
+  p.background_odd
+
+  ww_formatter = WorkingWeekFormatter.new(wweek)
+  p.calendar_column ww_formatter.to_month_calendar
+  year_calendar << ww_formatter.to_year_calendar
+  i += 1
+end
+
+# Blank even page
+p.start_new_page
+
+# Year view pages
+year_calendar = year_calendar.join("\n")
+p.font 'Inconsolata0.ttf', size: 6
+p.create_stamp_dots('dots_year', 14.5, 12)
+3.times do
+  [0, p.gutter_cm].each do |x|
+    p.start_new_page
+    [p.page_height, 0].each do |y|
+      p.stamp_at('dots_year', [x + p.hmargin_cm, y + p.vmargin_cm])
+      p.text_box year_calendar, at: [x + p.page_width - p.hmargin_cm - 7.cm,
+                                     y + p.page_height - p.hmargin_cm],
+                                width: 6.cm, height: p.page_height - p.hmargin_cm * 2,
+                                inline_format: true,
+                                align: :right, valign: :center
+    end
+  end
+end
+p.render_file("calendars/calendarA5_#{year}.pdf")
+`cmd /c start "" """calendars/calendarA5_#{year}.pdf"`
